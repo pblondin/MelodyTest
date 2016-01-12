d70d9c0528f0023ceac0adde3238e461ba5d4d45
Philippe Blondin <info@philippeblondin.ca>
2016-01-12 16:56:45 -0500

    Update project structure

--- MelodyTest/Controllers/MelodyRootViewController.m
+++ MelodyTest/Controllers/MelodyRootViewController.m
@@ -6,14 +6,22 @@
 //  Copyright © 2016 Philippe Blondin. All rights reserved.
 //
 
+#import "MelodiesManager.h"
 #import "MelodyRootViewController.h"
+#import "SoundPlayButton.h"
 
 @interface MelodyRootViewController ()
 
+// images
+@property(weak, nonatomic) IBOutlet UIImageView *bgImage;
+
 // buttons
-@property(weak, nonatomic) IBOutlet UIButton *playButton;
+@property(weak, nonatomic) IBOutlet SoundPlayButton *playButton;
 @property(weak, nonatomic) IBOutlet UIButton *clearButton;
 
+@end
+
+@interface MelodyRootViewController ()
 @end
 
 @implementation MelodyRootViewController
@@ -22,6 +30,7 @@
     [super viewDidLoad];
 
     // setup
+    [self registerNotifications];
     [self setupUI];
 }
 
@@ -30,7 +39,42 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - UI
+- (void)dealloc {
+    [self unregisterNotifications];
+}
+
+#pragma mark - Observers
+
+- (void)registerNotifications {
+    NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+    @weakify(self);
+    [center addObserverForName:MelodiesManagerDidStartPlaying
+                        object:nil
+                         queue:[NSOperationQueue mainQueue]
+                    usingBlock:^(NSNotification *_Nonnull note) {
+                      @strongify(self);
+                      // we make sure to update Play button
+                      [self.playButton setPlayButtonState:SoundPlayButtonStatePause];
+                    }];
+
+    [center addObserverForName:MelodiesManagerDidStopPlaying
+                        object:nil
+                         queue:[NSOperationQueue mainQueue]
+                    usingBlock:^(NSNotification *_Nonnull note) {
+                      @strongify(self);
+                      // we make sure to update Play button
+                      [self.playButton setPlayButtonState:SoundPlayButtonStatePlay];
+                    }];
+}
+
+- (void)unregisterNotifications {
+    NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+    [center removeObserver:self name:MelodiesManagerDidStartPlaying object:nil];
+    [center removeObserver:self name:MelodiesManagerDidStopPlaying object:nil];
+    [center removeObserver:self];
+}
+
+#pragma mark - Setup
 
 - (void)setupUI {
     // bg color
@@ -40,14 +84,22 @@
 #pragma mark - IB Actions
 
 - (IBAction)playMelodyButton:(id)sender {
-    DDLogDebug(@"press play melody button");
+    DDLogDebug(@"press play/pause melody button");
+
+    // toogle play/pause
+    self.playButton.selected = !self.playButton.selected;
 
     // toggle button
-    self.playButton.selected = !self.playButton.selected;
+    if (self.playButton.playButtonState == SoundPlayButtonStatePlay) {
+        [[MelodiesManager sharedInstance] playMelodies];
+    } else {
+        [[MelodiesManager sharedInstance] pauseMelodies];
+    }
 }
 
 - (IBAction)clearMelodyButton:(id)sender {
     DDLogDebug(@"press clear melody button");
+    [[MelodiesManager sharedInstance] clearMelodies];
 }
 
 @end
